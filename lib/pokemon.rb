7a88945ae850b1e07e0b7827d52fd3e6b6fa5698
Patrick A <akolo@hotmail.com>
2019-07-14 18:50:30 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,60 @@
 class Pokemon
+  attr_accessor :id, :name, :type
+  
+  def initialize(id: nil, name: 'Pikachu', type: 'electric', db:)
+    @id = id 
+    @name = name 
+    @type = type
+    @db = db 
+  end 
+  
+  def self.create_table
+    sql = <<-SQL 
+     CREATE TABLE IF NOT EXISTS pokemon (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      type TEXT, 
+      db TEXT
+      )
+    SQL
+   
+   DB[:conn].execute(sql) 
+  end
+  
+  def save
+    if self.id
+    else
+      sql = <<-SQL
+        INSERT INTO pokemons (name, type) 
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.type)
+
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemons")[0][0]
+    end 
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    type = row[2]
+    self.new(id, name, type)
+  end 
+  
+  def self.find(name)
+    # find the pokemon in the database given a name
+    # return a new instance of the Pokemon class
+    sql = <<-SQL
+      SELECT *
+      FROM Pokemons
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+ end 
 end
